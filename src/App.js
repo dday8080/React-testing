cf2684b6a2616b5006940a724a860f226e0549f2
Tommy <dday_8080@yahoo.com>
2022-07-26 03:28:19 -0600

    react testing 002

--- src/App.js
+++ src/App.js
@@ -1,9 +1,102 @@
 
 import './App.css';
+import {useState} from "react";
+let cc = console.log
 
-function MyButton() {
+const PRODUCTS = [
+    {category: 'Fruits', price: '$1', stocked: true, name: 'Apple'},
+    {category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit'},
+    {category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit'},
+    {category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach'},
+    {category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin'},
+    {category: 'Vegetables', price: '$1', stocked: true, name: 'Peas'},
+];
+
+function FilterableProductTable({products}) {
+    return(
+        <div>
+            <SearchBar />
+            <ProductTable products={products} />
+        </div>
+    );
+}
+
+function SearchBar () {
     return (
-        <button> Touch me!!! </button>
+        <form>
+            <input type="text" placeholder="search..." />
+            <label><br/>
+                <input type="checkbox"/>
+                {' '}
+                Only show products in stock
+            </label>
+        </form>
+    );
+}
+
+function ProductTable ({products}) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                category={product.category}
+                key={product.category} />
+            );
+        }
+        rows.push(
+            <ProductRow
+            product={product}
+            key={product.name} />
+        );
+        lastCategory = product.category;
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function ProductRow({product}) {
+    const name = product.stocked ? product.name :
+        <span style={{color: 'red'}}>
+            {product.name}
+        </span>
+    return(
+      <tr>
+          <td>{name}</td>
+          <td>{product.price}</td>
+      </tr>
+    );
+}
+
+function ProductCategoryRow ({category}) {
+    return(
+        <tr>
+            <th colSpan='2'>
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+
+function MyButton({count, onClick}) {
+
+    return (
+        <button onClick={onClick}>
+            Touched me {count} times
+        </button>
     );
 }
 function Header() {
@@ -15,16 +108,26 @@ function Header() {
     );
 }
 
+
 function App() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
   return (
 
     <div className="App">
         <Header />
         <div>
             <br/>
-            <MyButton />
-            <Avatar />
-            <Products />
+            <MyButton count={count} onClick={handleClick} />
+            <Avatar /><br/><br/>
+            <FilterableProductTable products={PRODUCTS} />
+            {/*<Products />*/}
+            {/*<MyButton count={count} onClick={handleClick} />*/}
+
         </div>
     </div>
     );
@@ -56,23 +159,23 @@ function Avatar() {
     );
 }
 
-function Products () {
-    const produce = [
-        {title: 'Cabbage', isFruit: false, id: 1},
-        {title: 'Garlic', isFruit: false, id: 2},
-        {title: 'Apple', isFruit: true, id: 3},
-    ];
-    const listItems = produce.map(produce =>
-        <li
-            key={produce.id}
-            style={{
-                color:produce.isFruit ? 'magenta' : 'darkgreen'
-            }}
-        >
-            {produce.title}
-        </li>
-    );
-    return(
-        <ul>{listItems}</ul>
-    );
-}
+// function Products () {
+//     const produce = [
+//         {title: 'Cabbage', isFruit: false, id: 1},
+//         {title: 'Garlic', isFruit: false, id: 2},
+//         {title: 'Apple', isFruit: true, id: 3},
+//     ];
+//     const listItems = produce.map(produce =>
+//         <li
+//             key={produce.id}
+//             style={{
+//                 color:produce.isFruit ? 'magenta' : 'darkgreen'
+//             }}
+//         >
+//             {produce.title}
+//         </li>
+//     );
+//     return(
+//         <ul>{listItems}</ul>
+//     );
+// }

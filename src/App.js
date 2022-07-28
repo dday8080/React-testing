ac7eda649148c5503f1e948a8792c69ac1b2ae11
dday8080 <dday_8080@yahoo.com>
2022-07-28 03:01:09 -0600

    exporting

--- src/App.js
+++ src/App.js
@@ -1,122 +1,10 @@
 
 import './App.css';
 import {useState} from "react";
+import FilterableP from "./FilterableP";
 let cc = console.log
 
-const PRODUCTS = [
-    {category: 'Fruits', price: '$1', stocked: true, name: 'Apple'},
-    {category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit'},
-    {category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit'},
-    {category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach'},
-    {category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin'},
-    {category: 'Vegetables', price: '$1', stocked: true, name: 'Peas'},
-];
 
-function FilterableProductTable({products}) {
-    const [filterText, setFilterText] = useState('');
-    const [inStockOnly, setInStockOnly] = useState(false)
-
-    return(
-        <div>
-            <SearchBar
-                filterText={filterText}
-                inStockOnly={inStockOnly}
-                onFilterTextChange={setFilterText}
-                onInStockOnlyChange={setInStockOnly} />
-            <ProductTable products={products}
-                filterText={filterText}
-                inStockOnly={inStockOnly} />
-        </div>
-    );
-}
-
-function SearchBar ({filterText, inStockOnly, onFilterTextChange,onInStockOnlyChange }) {
-
-    return (
-        <form>
-            <input
-                type="text"
-                value={filterText}
-                placeholder="search..."
-                onChange={(e) => onFilterTextChange(e.target.value)} />
-            <label><br/>
-                <input
-                    type="checkbox"
-                    checked={inStockOnly}
-                onChange={(e) => onInStockOnlyChange(e.target.value)} />
-                {' '}
-                Only show products in stock
-            </label>
-        </form>
-    );
-}
-
-function ProductTable ({products, filterText, inStockOnly}) {
-    const rows = [];
-    let lastCategory = null;
-
-    products.forEach((product) => {
-        if(
-            product.name.toLowerCase().indexOf(
-                filterText.toLowerCase()
-            ) === -1
-        ) {
-            return;
-        }
-        if (inStockOnly && !product.stocked) {
-            return;
-        }
-
-        if (product.category !== lastCategory) {
-            rows.push(
-                <ProductCategoryRow
-                category={product.category}
-                key={product.category} />
-            );
-        }
-        rows.push(
-            <ProductRow
-            product={product}
-            key={product.name} />
-        );
-        lastCategory = product.category;
-    });
-
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-        </table>
-    );
-}
-
-function ProductRow({product}) {
-    const name = product.stocked ? product.name :
-        <span style={{color: 'red'}}>
-            {product.name}
-        </span>
-    return(
-      <tr>
-          <td>{name}</td>
-          <td>{product.price}</td>
-      </tr>
-    );
-}
-
-function ProductCategoryRow ({category}) {
-    return(
-        <tr>
-            <th colSpan='2'>
-                {category}
-            </th>
-        </tr>
-    );
-}
 
 
 function MyButton({count, onClick}) {
@@ -144,20 +32,20 @@ function App() {
         setCount(count + 1);
     }
 
-  return (
+    return (
 
-    <div className="App">
-        <Header />
-        <div>
-            <br/>
-            <Avatar /><br/><br/>
-            <MyButton count={count} onClick={handleClick} />
-            <FilterableProductTable products={PRODUCTS} />
-            {/*<Products />*/}
-            {/*<MyButton count={count} onClick={handleClick} />*/}
+        <div className="App">
+            <Header/>
+            <div>
+                <br/>
+                <Avatar/><br/><br/>
+                <MyButton count={count} onClick={handleClick}/>
+                <FilterableP/>
+                {/*<Products />*/}
+                {/*<MyButton count={count} onClick={handleClick} />*/}
 
+            </div>
         </div>
-    </div>
     );
 
 }

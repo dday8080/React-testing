35dff4694199201587ca59a8b2ccf7eed58363c9
Tommy <dday_8080@yahoo.com>
2022-08-09 05:10:27 -0600

    testing page is In use

---
+++ src/junk.js
@@ -0,0 +1,32 @@
+{/*<Avatar*/}
+{/*    person={{*/}
+{/*        name: "Albert Einstein",*/}
+{/*        imageId: "Zg6Vcxs"*/}
+{/*    }}*/}
+{/*    size={40}*/}
+{/*/>*/}
+{/*✔*/}
+{/* <Avatar*/}
+{/*     person={{*/}
+{/*         name: "Albert Einstein",*/}
+{/*         imageId: "Zg6Vcxs"*/}
+{/*     }}*/}
+{/*     size={160}*/}
+{/* />*/}
+
+// function Avatar ({ person, size}) {
+//     let avatarSize = 's'
+//     if (size > 90) {
+//         avatarSize = 'b'
+//     }
+//
+//     return(
+//         <img
+//             className="avatar"
+//             src={getImageUrl(person, avatarSize)}
+//             alt={person.name}
+//             width={size}
+//             height={size}
+//         />
+//     )
+// }
--- src/routes/TestingStuff.js
+++ src/routes/TestingStuff.js
@@ -1,6 +1,70 @@
 import Nav from "./Nav";
 // import {getImageUrl} from "../FilterableP";
 
+const peeps = [{
+    id: 0,
+    name: 'Tom Callies JR',
+    relation: 'father'
+},{
+    id: 1,
+    name:'Amy Nelson',
+    relation:'mother',
+},{
+    id: 2,
+    name: 'Thomas Callies III',
+    relation: "father",
+},{
+    id: 3,
+    name:"Amanda Callies",
+    relation:'mother',
+},{ id: 4,
+    name:'Thomas Callies IV',
+    relation:'son',
+},{ id: 5,
+    name:'Troy Callies',
+    relation:'son',
+},{ id: 6,
+    name:'Ava Callies',
+    relation:'daughter',
+}];
+
+function Family(){
+    // const fathers = peeps.filter(person =>
+    //     person.relation === "mother"
+    // );
+    const listPeeps = peeps.map(person =>
+        <li key={person.id} className="familyLi">{person.name}</li>
+    );
+    return(
+        <div>
+            <div className="expenseFlexboxInput">
+                <div className="expenseFlexboxOutputHeader">
+                    <h1>Family</h1>
+                </div>
+                <ul className="linkStyle"> {listPeeps}</ul>
+            </div>
+        </div>
+    )
+}
+
+function ItemCarrier(){
+    return(
+        <div>
+            <Item
+                isPacked={true}
+                name="Space Suit"
+            />
+            <Item
+                isPacked={false}
+                name="Space Helmet"
+            />
+            <Item
+                isPacked={true}
+                name="Space Boots"
+            />
+        </div>
+    )
+}
 function Item({name, isPacked}) {
 
     return (
@@ -17,19 +81,10 @@ function Testing(){
     return(
         <div className="App">
             <Nav />
-            <h2>Ready for space ✔</h2>
-            <Item
-                isPacked={true}
-                name="Space Suit"
-            />
-            <Item
-                isPacked={false}
-                name="Space Helmet"
-            />
-            <Item
-                isPacked={true}
-                name="Space Boots"
-            />
+            <h2>Ready for Testing ✔</h2>
+            <ItemCarrier/>
+            <Family/>
+
 
         </div>
     )

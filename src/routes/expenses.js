01f51dbfeccd6b60aa1f7b4db38117f794c35784
Tommy <dday_8080@yahoo.com>
2022-10-01 01:02:47 -0600

    testing page is In use

--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -2,76 +2,67 @@ import Nav from "./Nav";
 import {useState} from "react";
 let cc = console.log
 
-function ExpenseFlexBox(){
+let nextId = 0;
 
-    return(
-        <div className="flexboxContainer">
-            <ExpenseInputContainer/>
-            <ExpenseOutput/>
-        </div>
-    )
-}
+function Expenses(){
+    const [expenseNameState, setExpenseNameState] = useState([""]);
+    const [expenseCostState, setExpenseCostState] = useState([]);
 
-function  ExpenseInputContainer(){
-
-    return(
-        <div className="expenseFlexboxInput">
-            <InputHeading/>
-            <ExpenseForm />
-        </div>
-    )
-}
-
-function InputHeading(){
-
-    return(
+    let InputHeading = (
         <h1 className="expenseTextBold">
             <span>Expenses</span><br/>
         </h1>
-    )
-}
+    );
 
-function ExpenseForm(){
-    const [expenseNameState, setExpenseNameState] = useState("Rent");
-    const [expenseCostState, setExpenseCostState] = useState(600);
-
-
-    return(
+    let ExpenseForm = (
         <div>
             <form id="expenseForm">
                 <li className='navBarLiA'>
                     <span>Name of Expense</span><br/>
                     <input type="text" value={expenseNameState} id="expenseName"
-                           onChange={(e) => setExpenseNameState(e.target.value)} /><br/><br/>
+                           onChange={(e) => {
+                               setExpenseNameState(e.target.value)
+                           }} /><br/><br/>
                     <span>Cost of Expense</span><br/>
                     <input type="number" value={expenseCostState} id="expenseCost"
-                    onChange={(e) => setExpenseCostState(e.target.value) } /><br/><br/>
-                    <input type="button" value='submit' id="expenseInput" onClick={(e) => {
-                        handlesOnClick(expenseNameState, expenseCostState)}} />
+                           onChange={(e) => setExpenseCostState(+e.target.value) } /><br/><br/>
+                    <input type="button" value='submit' id="expenseInput"
+                           onClick={(e) => {
+                               setExpenseNameState([
+                                   ...expenseNameState,
+                                   {id: nextId++, name: expenseNameState}
+                               ]);
+                               setExpenseCostState([
+                                   ...expenseCostState,
+                                   {id: nextId++, name: expenseCostState}
+                               ]);
+                                handleStoringStates(setExpenseNameState,setExpenseCostState);}} />
                 </li>
             </form>
         </div>
-    )
-}
+    );
 
-function handlesOnClick(expenseNameState, costOfExpense){
+    let expenseInputContainer = (
+        <div className="expenseFlexboxInput">
+            {InputHeading}
+            {ExpenseForm}
+            <button onClick={(e) => {
+                cc(expenseNameState);
+                cc(typeof(expenseNameState))
+            }}>Log</button>
+        </div>
+    );
 
-ExpenseNamedItem(expenseNameState)
-}
-
-function ExpenseOutput(){
-
-
-    return(
+    let ExpenseOutput = (
         <div className="flexboxContainer">
             <div className="expenseFlexboxInput">
                 <div className="expenseFlexboxOutputHeader" >
-                  <h1 className="expenseTextBold">
-                      <span>Name</span><br/>
-                  </h1>
+                    <h1 className="expenseTextBold">
+                        <span>Name</span><br/>
+                    </h1>
                 </div>
                 <div className="expenseFlexboxOutputItem">
-                    <ExpenseNamedItem/>
+                    <ExpenseNamedItem expenseNameState={expenseNameState}/>
                 </div>
             </div>
 
@@ -82,34 +73,54 @@ function ExpenseOutput(){
                     </h1>
                 </div>
                 <div className="expenseFlexboxOutputItem">
-                    <span> Some Stuff</span>
+                    <ExpenseOfItem expenseCostState={expenseCostState}/>
                     <span>More Stuff</span>
                 </div>
             </div>
         </div>
-    )
-}
- function ExpenseNamedItem(expenseNameState){
-let namedExpense = expenseNameState;
-    cc(namedExpense);
-    cc(typeof namedExpense);
-    return(
-         <li>
-             {namedExpense}
-         </li>
-
-    )
-}
+    );
 
 
-export default function Expenses (){
 
-    return(
+    return (
         <div className="App">
             <Nav />
             <h2 style={{color: "white"}}> Expenses </h2>
-            <ExpenseFlexBox />
+                <div className="flexboxContainer">
+                    {expenseInputContainer}
+                    {ExpenseOutput}
+                </div>
 
+        </div>
+    );
+}
+function ExpenseOfItem({expenseCostState}) {
+    cc(expenseCostState);
+    return(
+        <div>
+            {expenseCostState}
         </div>
     )
 }
+
+function ExpenseNamedItem({expenseNameState}) {
+    cc(expenseNameState)
+
+    return (
+        <div>
+            {expenseNameState}
+        </div>
+    )
+}
+function handleStoringStates(setExpenseNameState, setExpenseCostState){
+
+
+    // localStorage.setItem("Name", expenseNameState);
+    // localStorage.setItem('Cost', expenseCostState);
+    // cc(localStorage.getItem("Name"));
+    // cc(localStorage.getItem("Cost"));
+}
+
+
+
+export default Expenses;
